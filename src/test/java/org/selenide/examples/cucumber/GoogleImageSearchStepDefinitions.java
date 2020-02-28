445dc4db6bf0c29019c6a8df580a997a80ef9c25
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-02-28 18:07:24 +0200

    fix Gherkin format (which git broken after last upgrade to newest Cucumber)

--- src/test/java/org/selenide/examples/cucumber/GoogleImageSearchStepDefinitions.java
+++ src/test/java/org/selenide/examples/cucumber/GoogleImageSearchStepDefinitions.java
@@ -11,20 +11,20 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class GoogleImageSearchStepDefinitions {
-  String keyword;
+  private String keyword;
   
   @When("click \"Images\" link")
   public void chooseImagesAsSearchTarget() {
     $(byText("Images")).click();
   }
 
-  @When("enter a keyword (.*) in input field")
+  @When("enter a keyword {string} in input field")
   public void enterKeyword(String keyword) {
     this.keyword = keyword;
     $(By.name("q")).val(keyword).pressEnter();
   }
 
-  @Then("at least top (\\d+) matching images should be shown")
+  @Then("at least top {int} matching images should be shown")
   public void topTenMatchedImagesShouldBeShown(int resultsCount) {
     $$(".rg_di.rg_el").shouldHave(sizeGreaterThanOrEqual(resultsCount));
     $(".rg_di.rg_el").find("img.rg_i").shouldHave(attribute("alt", "Image result for " + keyword));
--- src/test/java/org/selenide/examples/cucumber/GoogleSearchStepDefinitions.java
+++ src/test/java/org/selenide/examples/cucumber/GoogleSearchStepDefinitions.java
@@ -11,26 +11,23 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleSearchStepDefinitions {
-  private String keyword;
-  
   @Given("an open browser with google.com")
   public void openGoogleSearch() {
     Configuration.reportsFolder = "target/surefire-reports";
     open("https://google.com/ncr");
   }
 
-  @When("a keyword (.*) is entered in input field")
+  @When("a keyword {string} is entered in input field")
   public void enterKeyword(String keyword) {
-    this.keyword = keyword;
     $(By.name("q")).val(keyword).pressEnter();
   }
 
-  @Then("at least top (\\d+) matches should be shown")
+  @Then("at least top {int} matches should be shown")
   public void topTenMatchesShouldBeShown(int resultsCount) {
     $$("#res .g").shouldHave(sizeGreaterThanOrEqual(resultsCount));
   }
 
-  @Then("the first one should contain (.+)")
+  @Then("the first one should contain {string}")
   public void theFirstOneShouldContainKeyword(String expectedText) {
     $("#res .g").shouldHave(text(expectedText));
   }
