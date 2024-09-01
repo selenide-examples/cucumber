ea50a672f64a2e36d4764a50b63d601970f7b337
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-09-01 11:18:57 +0300

    add chrome argument to overcome robot protection
    "--disable-blink-features=AutomationControlled"

--- src/test/java/org/selenide/examples/cucumber/TextReport.java
+++ src/test/java/org/selenide/examples/cucumber/TextReport.java
@@ -1,12 +1,22 @@
 package org.selenide.examples.cucumber;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SimpleReport;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
 
 public class TextReport {
   private final SimpleReport report = new SimpleReport();
+
+  @BeforeAll
+  public static void beforeAllTests() {
+    Configuration.browserCapabilities = new ChromeOptions()
+      .addArguments("--disable-blink-features=AutomationControlled");
+  }
 
   @Before
   public void beforeTest(Scenario scenario) {
